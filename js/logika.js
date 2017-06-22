1e32c2f2c8197b5e8247c2d4160d61fe3d764ca3
marcinmika1 <marcinmika@hotmail.com>
2017-06-22 16:59:23 +0200

    szukanie js

---
+++ js/logika.js
@@ -0,0 +1,13 @@
+document.getElementsByClassName('jeden').onclick= function(){wstawianie()};
+
+function wstawianie(){
+     document.getElementsByClassName('to').innerHTML="1";
+}
+    
+    
+    
+    
+    
+    
+    
+   
